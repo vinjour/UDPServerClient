ae3ad8204e34db0815ffb79fcebd852d8f8263d0
vinjour <jourdren.vincent@gmail.com>
2022-11-30 17:33:50 +0100

    UDP Server and client

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -1,10 +1,12 @@
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class UDPClient {
     static int BUFFER_SIZE = 1024;
     static int serverPort;
+    static InetAddress serverAddress;
     DatagramSocket clientSocket;
     DatagramPacket receivePacket;
     BufferedReader inFromUser;
@@ -21,54 +23,53 @@ public class UDPClient {
 
         //On récupere la ligne courante
         inFromUser = new BufferedReader(new InputStreamReader(System.in));
-
-
-        byte[] sendData = new byte[BUFFER_SIZE];
+        sendData = new byte[BUFFER_SIZE];
         receiveData = new byte[BUFFER_SIZE];
-
+        receivePacket =  new DatagramPacket(receiveData, receiveData.length);
     }
 
     public void run(InetAddress serverAddress) throws IOException {
 
-        while(true) {
-            System.out.print("Enter Message: ");
+        boolean keepListening = true;
 
-            //On récupère le message
-            String sentence = inFromUser.readLine();
-            sendData = sentence.getBytes();
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
+        while(keepListening) {
 
-            //On envoi le message
-            clientSocket.send(sendPacket);
+            sendMessage(serverAddress);
 
-            //On prépare la reception du 1er message
-            DatagramPacket receivePacket =  new DatagramPacket(receiveData, receiveData.length);
-
-
-            System.out.println ("Waiting for return packet");
             // delai maximal d'attente 10000 ms
-            clientSocket.setSoTimeout(10000);
+            //clientSocket.setSoTimeout(10000);
 
-            try {
-                clientSocket.receive(receivePacket);
-
-                receivePacket.getData();
-                int l = receivePacket.getLength();
-                byte[] cutData = Arrays.copyOf(receiveData, l);
-                String echoSentence = new String(cutData);
-
-                System.out.println("From server at: " + serverAddress + ":" + serverAddress);
-                System.out.println("Message: " + echoSentence);
-            } catch (SocketTimeoutException ste) {
-                System.out.println("Timeout Occurred: Packet assumed lost");
-            }
+            receiveMessage(serverAddress, receivePacket);
         }
-        // clientSocket.close();
+        clientSocket.close();
+    }
+
+    private void sendMessage(InetAddress serverAddress) throws IOException {
+        System.out.print("Enter Message: ");
+
+        String sentence = inFromUser.readLine();
+        sendData = sentence.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
+
+        clientSocket.send(sendPacket);
+    }
+
+    private void receiveMessage(InetAddress serverAddress, DatagramPacket receivePacket) throws IOException {
+
+        System.out.println ("Waiting for return packet");
+
+        clientSocket.receive(receivePacket);
+        int packetLength = receivePacket.getLength();
+        System.out.print(packetLength);
+        String echoSentence = new String(receiveData, 0, packetLength, StandardCharsets.UTF_8);
+
+        System.out.println("From server at: " + serverAddress + ":" + serverPort);
+        System.out.println("Message: " + echoSentence + "\n");
     }
 
     public static void main(String args[]) throws Exception {
 
-        InetAddress serverAddress = InetAddress.getByName(args[0]);
+        serverAddress = InetAddress.getByName(args[0]);
         UDPClient.serverPort = Integer.parseInt(args[1]);
 
         UDPClient udpClient = new UDPClient();
